ee6ceb26ed009a755c48225d9794f5032a15a335
RCHOUATT <cheickhamedouattara865@gmail.com>
2024-10-05 12:08:17 +0000

    Modification de la page formation Langue part1

--- src/app/composants/formations/langue/langue.component.ts
+++ src/app/composants/formations/langue/langue.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {ButtonComponent} from "../../button/button.component";
+import {RouterLink} from "@angular/router";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-langue',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonComponent,
+    RouterLink,
+    NgClass
+  ],
   templateUrl: './langue.component.html',
-  styleUrl: './langue.component.scss'
+  styleUrl: './langue.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LangueComponent {
+
+  isDropdownOpen = true;
+  activeCours: string = 'Cours 1'; // Par défaut
+  activeLesson: string = 'Lesson 1';
+
+
+  hideDropdown() {
+    this.isDropdownOpen = false;
+  }
+  openDropdown() {
+    this.isDropdownOpen = true;
+  }
+
+
+  setActive(menuItem: string) {
+    this.activeCours = menuItem;
+  }
+  setActiveLesson(lesson: string) {
+    this.activeLesson = lesson;
+  }
 
 }
